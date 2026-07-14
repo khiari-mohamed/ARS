9257b47368afc80d51bc7287a61648285a41828a
khiari-mohamed <mouhamed.khiari.93093292@gmail.com>
2026-07-14 18:03:37 +0100

    clean up

--- server/scripts/upload-check-scripts.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Uploading check scripts to server...
-scp check-db-schema.js arsadmin@arshosting:/home/yourapp/server/
-scp check-prisma-client.js arsadmin@arshosting:/home/yourapp/server/
-echo Done!
-pause
